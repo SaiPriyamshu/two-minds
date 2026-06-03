8dca620cf52a4638f5d30e1666b79b9f93d545e8
Shamindra17 <shamindra2006@gmail.com>
2026-06-03 13:30:32 +0530

    Initial MVP

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Two Minds</h1>
+      <p>Your Career. Your Roadmap.</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
